fa4bbecbf26b3d9de949d1f3f6f278032f3d4ff6
Blaine Schanfeldt <blaine@technologyaz.com>
2011-02-04 12:04:48 -0800

    enforce a strict timeout (trying to figure out what the int means)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,6 @@ class ItemsController < ApplicationController
     @item = Item.find_or_initialize_by(:posting_id => posting_id)
     if @item.new_record?
       agent = Mechanize.new
-      agent.open_timeout(1000)
-      agent.read_timeout(1000)
       z = agent.get(link)
       t = z.search("h2").first.text.match(/\$([1234567890\.]+)\s/)
       price = t[1] if t
