fd18b74301a6e800e5dc5fbc635bd9e50e652994
Blaine Schanfeldt <blaine@technologyaz.com>
2011-02-04 04:03:20 -0800

    adding caching to items response

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
     if @item.new_record?
       agent = Mechanize.new
       z = agent.get(link)
-      hash = {:title => z.search("h2").first.text, :href => link, :text => z.search("#userbody").first.text, :posting_id => posting_id}
+      hash = {:title => z.search("h2").first.text, :price => z.search("h2").first.text.match(/\$([1234567890\.]+)\s/)[1], :href => link, :text => z.search("#userbody").first.text, :posting_id => posting_id}
       images = z.search("img")
       if images.size > 0
         hash[:img] = images[1]['src'] if images.size > 1
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
       @item.save
     end
     
-
+    response.headers['Cache-Control'] = 'public, max-age=3600'
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @item }
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item
   include Mongoid::Document
   
-  field :posting_id
+  field :posting_id, :type => Integer
   field :title
   field :href
   field :text
   field :img
-  
+  field :price
 end
